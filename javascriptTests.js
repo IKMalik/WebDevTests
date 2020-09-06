7408b82aa090551297f535c50b8174b42a9f8c84
Ibrahim Malik <ibstar.malik@gmail.com>
2020-09-06 21:01:16 +0100

    Updated css, added simple js

---
+++ javascriptTests.js
@@ -0,0 +1,14 @@
+
+// variable decl dont use var due to scope behaviour 
+// better to use const unless you are sure var needs to be changed 
+// const is still mutable just not reassignable 
+
+
+// core syntax same as python for primitve behavour 
+
+const inputt = window.prompt("Enter your name");
+
+// event handler 
+function handleClick(element){
+	alert('You pressed me !');
+}
